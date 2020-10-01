8914658d8681e27af16ce1fec008d3ddf15277d1
ghassen@tanitlab.com <ghassen@tanitlab.com>
2020-10-01 11:18:32 +0100

    second push

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app';
+//   name: string= "ghasaaaaaaaaaaaaaa";
+//   url ='http://35.239.35.192:32001/hello_user'
+//   constructor(
+//     public http: Http, private router: Router) {
+//      this.getContact(this.name).subscribe(user => {
+//       if(!user){
+//         return;
+//       }
+//       // this.listData = new MatTableDataSource(user.data);
+//       this.s=user.data
+//   });
+//
+//   }
+//   getContact(x) {
+//     return this.http.post('http://35.239.35.192:32001/hello_user', {
+//       name:  x,
+//       }).map(res => res.json());
+//   }
+
+
+    title = "getContact();"
+
 }
